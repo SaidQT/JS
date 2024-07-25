0466d4ab21663fb83f2d7ff582d668f3551b904c
saeed <saidqtaish@gmail.com>
2024-07-25 13:20:45 +0300

    adding sensei assignment

--- ninja.js
+++ ninja.js
@@ -20,6 +20,24 @@ class Ninja{
         this.health+=10
     }
 }
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
+class Sensei extends Ninja{
+  
+    constructor(name){
+        super(name);
+        this.health=200;
+        this.speed=10;
+        this.strength=10;
+        this.wisdom=10;
+    }
+    callWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
